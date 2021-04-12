458f770ad5f96111c894eb787bf3da8484317f64
tcyang <tcyang@gm.pu.edu.tw>
2021-04-12 11:43:23 +0800

    手勢切換圖片

--- app/src/main/java/tw/edu/pu/cs/tcyang/pu/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/cs/tcyang/pu/MainActivity.kt
@@ -11,11 +11,42 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
 
     lateinit var gDetector: GestureDetector
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var PictureNo:Int = 0  //目前顯示第幾張圖
+    var TotalPictures:Int = 0 //總共幾張圖片(假設僅顯示pu0-pu3)
+
+
+    fun ShowPicture() {
+        /*
+        when (PictureNo) {
+            0 -> img.setImageResource(R.drawable.pu0)
+            1 -> img.setImageResource(R.drawable.pu1)
+            2 -> img.setImageResource(R.drawable.pu2)
+            3 -> img.setImageResource(R.drawable.pu3)
+        }
+
+         */
+        var res:Int = getResources().getIdentifier("pu" + (PictureNo),
+            "drawable", getPackageName())
+        img.setImageResource(res)
+
+        txv.text = PictureNo.toString()
+    }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gDetector = GestureDetector(this,this)
-    }
+
+            var res:Int = -1
+            var countDrawables:Int = -1
+            while (res != 0) {
+                countDrawables++;
+                res = getResources().getIdentifier("pu" + (countDrawables),
+                    "drawable", getPackageName());
+            }
+            TotalPictures = countDrawables
+
+        }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         /*
@@ -35,18 +66,20 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
 
     override fun onShowPress(e: MotionEvent?) {
         //TODO("Not yet implemented")
-        txv.text = "按下後無後續動作"
+        //txv.text = "按下後無後續動作"
     }
 
     override fun onSingleTapUp(e: MotionEvent?): Boolean {
         //TODO("Not yet implemented")
-        txv.text = "短按"
+        //txv.text = "短按"
+        PictureNo = 0
+        ShowPicture()
         return true
     }
 
     override fun onDown(e: MotionEvent?): Boolean {
         //TODO("Not yet implemented")
-        txv.text = "按下"
+        //txv.text = "按下"
         return true
     }
 
@@ -57,9 +90,19 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
         velocityY: Float
     ): Boolean {
         //TODO("Not yet implemented")
-        txv.text = "快滑\nx1y1: " + e1.getX().toString() + ", " + e1.getY().toString() +
-                "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString() +
-                "\nX軸Y軸速度:" + velocityX.toString() + ", " +  velocityY.toString()
+        //txv.text = "快滑\nx1y1: " + e1.getX().toString() + ", " + e1.getY().toString() +
+        //        "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString() +
+        //        "\nX軸Y軸速度:" + velocityX.toString() + ", " +  velocityY.toString()
+        if (e1.getX() < e2.getX()){  //向右快滑
+            PictureNo++
+            if (PictureNo == TotalPictures) {PictureNo = 0}
+        }
+        else{     //向左快滑
+            PictureNo--;
+            if (PictureNo < 0) {PictureNo = TotalPictures - 1 }
+        }
+        ShowPicture()
+
         return true
     }
 
@@ -70,13 +113,15 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
         distanceY: Float
     ): Boolean {
         //TODO("Not yet implemented")
-        txv.text = "拖曳\nx1y1: " +  e1.getX().toString() + ", " + e1.getY().toString() +
-                "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString()
+        //txv.text = "拖曳\nx1y1: " +  e1.getX().toString() + ", " + e1.getY().toString() +
+        //        "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString()
         return true
     }
 
     override fun onLongPress(e: MotionEvent?) {
         //TODO("Not yet implemented")
-        txv.text = "長按"
+        PictureNo = TotalPictures - 1
+        ShowPicture()
+        //txv.text = "長按"
     }
 }
